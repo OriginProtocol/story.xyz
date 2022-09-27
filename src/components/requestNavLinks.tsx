b746917679189af454abee006e692e25bcb5c680
Nick Hong <nickhong@originprotocol.com>
2022-09-27 13:21:20 -0400

    Go back to having no cms integration

--- src/components/requestNavLinks.tsx
+++
@@ -1,7 +0,0 @@
-const requestNavLinks = async () => {
-  const data = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/cms`)
-  const dataParsed = await data.json()
-  return { links: dataParsed }
-}
-
-export default requestNavLinks
