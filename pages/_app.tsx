38506f3db78d805b5e8060f8eb7df6b8030b8102
rolandpo <rolopod@gmail.com>
2022-12-20 17:31:33 +0100

    remove ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,20 +4,8 @@ import '../src/styles/globals.css'
 import '@originprotocol/origin-storybook/lib/styles.css'
 import type { AppProps, AppContext } from 'next/app'
 import Script from 'next/script'
-import * as gtag from "../src/helpers/gtag"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <>
       <Script id="google-tag-manager" strategy="afterInteractive">
@@ -32,19 +20,6 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
-      />
-      <Script
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
       />
       <Component {...pageProps} />
     </>
